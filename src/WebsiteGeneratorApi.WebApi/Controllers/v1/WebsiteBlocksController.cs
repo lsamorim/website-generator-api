a58c841c9d4d714e8baf45c9acaa756b0ae1db9d
Lucas Amorim <lsamorim.it@gmail.com>
2023-10-30 20:02:53 -0300

    Implement an endpoint for creating website blocks

---
+++ src/WebsiteGeneratorApi.WebApi/Controllers/v1/WebsiteBlocksController.cs
@@ -0,0 +1,42 @@
+﻿using Application.Services.Implementations;
+using Application.Services.Interfaces;
+using Application.UseCases.Implementations.CreateWebsiteBlocks.Models;
+using Application.UseCases.Interfaces;
+using Domain;
+using Domain.Components.Blocks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Text.Json;
+
+namespace WebsiteGeneratorApi.WebApi.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class WebsiteBlocksController : ControllerBase
+    {
+        private readonly IBlockTypeMapper _blockTypeMapper;
+
+        public WebsiteBlocksController(IBlockTypeMapper blockTypeMapper)
+        {
+            _blockTypeMapper = blockTypeMapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAsync(
+            [FromQuery] string key,
+            [FromBody] List<dynamic> blocks,
+            [FromServices] ICreateWebsiteBlocksUseCase useCase,
+            CancellationToken cancellationToken)
+        {
+            var blocksList = _blockTypeMapper.Map(blocks);
+
+            var input = new CreateWebsiteBlocksInput(blocksList);
+
+            await useCase.ExecuteAsync(input, cancellationToken);
+
+            var result = JsonConvert.SerializeObject(input.Blocks);
+            return Ok(result);
+        }
+    }
+}
